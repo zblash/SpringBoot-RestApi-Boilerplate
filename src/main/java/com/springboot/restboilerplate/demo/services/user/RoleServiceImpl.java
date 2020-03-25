4714d0530515bf05865e7001eae8c0658bcad219
Yusuf Can Celik <yusufcancelik@hotmail.com>
2020-03-25 03:35:41 +0300

    role service implemented

--- src/main/java/com/springboot/restboilerplate/demo/services/user/RoleServiceImpl.java
+++ src/main/java/com/springboot/restboilerplate/demo/services/user/RoleServiceImpl.java
@@ -1,17 +1,21 @@
 package com.springboot.restboilerplate.demo.services.user;
 
+import com.springboot.restboilerplate.demo.configs.constants.MessagesConstants;
+import com.springboot.restboilerplate.demo.errors.ResourceNotFoundException;
 import com.springboot.restboilerplate.demo.models.Role;
 import com.springboot.restboilerplate.demo.repositories.RoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RoleServiceImpl implements RoleService {
 
     @Autowired
     private RoleRepository roleRepository;
+
 
     @Override
     public List<Role> findAll() {
@@ -20,17 +24,18 @@ public class RoleServiceImpl implements RoleService {
 
     @Override
     public Role findByName(String name) {
-        return null;
+        return roleRepository.findByName(name).orElseThrow(() -> new ResourceNotFoundException(MessagesConstants.RESOURCES_NOT_FOUND+"role", name));
     }
 
     @Override
     public Role create(Role role) {
-        return null;
+        return roleRepository.save(role);
     }
 
     @Override
     public Role update(Role role, Role updatedRole) {
-        return null;
+        role.setName(updatedRole.getName());
+        return roleRepository.save(role);
     }
 
     @Override
@@ -38,8 +43,15 @@ public class RoleServiceImpl implements RoleService {
         roleRepository.delete(role);
     }
 
-    @Override
-    public Role createOrFind(String roleName) {
-        return null;
+    public Role createOrFind(String roleName){
+        Optional<Role> optionalRole = roleRepository.findByName(roleName);
+        if (optionalRole.isPresent()){
+            return optionalRole.get();
+        }
+        Role role = new Role();
+        role.setName(roleName);
+        return create(role);
+
+
     }
 }
